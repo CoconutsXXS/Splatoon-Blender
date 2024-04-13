0c7a842c9391921afefd7ca11fd97463bba1fcb8
Coconuts XXS <123900385+CoconutsXXS@users.noreply.github.com>
2024-04-13 21:19:13 +0200

    Modules Again >:(

--- 3D.js
+++ 3D.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import * as THREE from './node_modules/three/build/three.module';
+import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js'
 
 
 function asyncTimeout(time)
