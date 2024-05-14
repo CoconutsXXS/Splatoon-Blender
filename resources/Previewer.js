c0a20b2017d83718b512a93a4c6242ddb5515733
Coconuts XXS <123900385+CoconutsXXS@users.noreply.github.com>
2024-05-14 21:09:26 +0200

    Map

--- resources/Previewer.js
+++ resources/Previewer.js
@@ -1,7 +1,8 @@
 import * as THREE from 'https://unpkg.com/three/build/three.module.js';
 import { ColladaLoader } from '../ColladaLoader.js';
 
-const startPath = 'https://coconutsxxs.github.io/Splatoon-Blender'
+// const startPath = 'https://coconutsxxs.github.io/Splatoon-Blender'
+const startPath = '../'
 
 Array.prototype.max = function() {
     return Math.max.apply(null, this);
